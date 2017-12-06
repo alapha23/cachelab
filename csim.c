75067a1f41b32b53207cb4dd27d9c493cd286f0a
alapha23 <alapha23@gmail.com>
2017-12-06 16:02:27 +0900

    Add parseArgs prototype

--- csim.c
+++ csim.c
@@ -1,7 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "cachelab.h"
 
-int main()
+/*
+ * **********************************************
+ * t bits    |     s      |      b      |
+ * *********************************************
+ */
+
+int s = 0;	// set index bits
+int E = 0;	// lines per set
+int b = 0;	// number of block offset bits
+trans_func_t request;
+
+void parseArgs(int argc, char **argv);
+
+int main(int argc, char **argv)
 {
+    parseArgs(argc, argv);
+
+
     printSummary(0, 0, 0);
     return 0;
 }
+
+void printSummary(int hits, int misses, int evictions)
+{
+
+
+}
+
+void initMatrix(int M, int N, int A[N][M], int B[M][N])
+{
+
+}
+
+void correctTrans(int M, int N, int A[N][M], int B[M][N])
+{
+
+}
+
+void registerTransFunction(void (*trans)(int M,int N,int[N][M],int[M][N]), char* desc)
+{
+
+
+}
+
+void parseArgs(int argc, char **argv)
+{
+
+case
+
+	-h 
+	printf
+	help
+
+	-v
+	verbose display trace info
+
+	-s  <s>
+	number of set index bits. S =2^s
+	-E <E>
+	-b <b>
+	-t <tracefile>
+
+
+}
+
